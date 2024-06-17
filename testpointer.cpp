21117860c8c39461deb82f0baa2e5b913492ab72
Sayeed <mssalam62@yahoo.com.au>
2024-06-17 15:45:07 +1000

    First Commit

---
+++ testpointer.cpp
@@ -0,0 +1,28 @@
+// classes example
+#include <iostream>
+using namespace std;
+
+class Rectangle
+{
+    int width, height;
+
+public:
+    Rectangle(int w, int h) : width(w), height(h)
+    {
+    }
+    Rectangle()
+    {
+        width = 2;
+        height = 2;
+    }
+    int area() { return width * height; }
+};
+
+int main()
+{
+    Rectangle rect(3, 4), yy;
+
+    cout << "area: " << rect.area();
+    cout << "area: " << yy.area();
+    return 0;
+}
